de63484b9fb47f2fe8b4603927682c4aaa1eb0f4
polo <lg550055@hotmail.com>
2022-01-25 15:49:46 -0600

    WIP

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_URL);
@@ -23,6 +24,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/books', handleGetBooks);
+app.post('/books', handlePostBooks);
+app.delete('books/:id', handleDeleteBooks);
 
 const Book = require('./models/book');
 
@@ -40,6 +43,25 @@ async function handleGetBooks(req, res) {
     }
   } catch(err) {
     res.status(500).send('Server error');
+  }
+}
+
+async function handlePostBooks(req, res) {
+  try {
+    const bookAdded = await Book.create(req.body);
+    res.status(201).send(bookAdded);
+  } catch(err) {
+    res.status(500).send('Server Error');
+  }
+}
+
+async function handleDeleteBooks(req, res) {
+  let id = req.params.id;
+  try {
+    await Book.findByIdAndDelete(id);
+    res.status(204).send('Book deleted');
+  } catch(err) {
+    res.status(404).send('Unable to delete '+id);
   }
 }
 
