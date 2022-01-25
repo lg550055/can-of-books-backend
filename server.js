f3ad3ddd2dc36f72f2e34fba63ddbfa18e682907
polo <lg550055@hotmail.com>
2022-01-24 22:22:59 -0600

    Adds basic db functionality per lab 11

--- server.js
+++ server.js
@@ -22,10 +22,6 @@ app.get('/', (req, res) => {
   res.send('server request received');
 });
 
-app.get('/cats', (req, res) => {
-  res.send('Cats route here');
-});
-
 app.get('/books', handleGetBooks);
 
 const Book = require('./models/book');
